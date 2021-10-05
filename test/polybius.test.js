686cfae2e926f62bb95cecd4ba31b465e2a03813
Paul Reynolds <usuallypaulreynolds@gmail.com>
2021-10-05 16:01:03 -0500

    Tests!

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -2,13 +2,20 @@
 const expect = require("chai").expect;
 const polybius = require("../src/polybius").polybius;
 
-describe("polybius", () => {
+describe("polybius written by Paul", () => {
     it("should make 22 into G", () => {
         let expected = 'g';
         let actual = polybius('22', false);
         expect(actual).to.equal(expected);
     });
     it("should change coded messages back into decoded ones", () => {
-        
+        let expected = "yay";
+        let actual = polybius('451145', false);
+        expect(actual).to.equal(expected);
+    })
+    it("should maintain spaces", () => {
+        let expected = "22 22";
+        let actual = polybius('g g', true);
+        expect(actual).to.equal(expected);
     })
 })
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,17 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const substitution = require("../src/substitution").substitution;
+
+describe("substitution written by Paul", () => {
+    it("should correctly transpose letters", () => {
+        let alphabet = 'plmoknijbuhvygctfxrdzeswaq';
+        let expected = 'ppp';
+        let actual = substitution('aaa', alphabet);
+        expect(actual).to.equal(expected);
+    });
+    it("should return false if there are repeated characters", () => {
+        let alphabet = 'aaa';
+        let expected = false;
+        let actual = substitution('aaa', alphabet);
+        expect(actual).to.equal(expected);
+    })
+})
